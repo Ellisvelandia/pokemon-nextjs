ca5afbf3cffc6fa9e619db51602d1fe7073a3f84
ellisvelandia <eyis619@gmail.com>
2022-10-22 21:26:24 -0500

    pokemons

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,55 @@
+import Layout from "../components/Layout";
+import { useState } from "react";
+import Pokemon from "../components/Pokemon";
+
 export default function Home({ initialPokemon }) {
   console.log(initialPokemon);
+  const [pokemon, setPokemon] = useState(initialPokemon);
+  const [offset, setOffet] = useState(0);
+
+  const fetchPokemon = async (url, next) => {
+    const response = await fetch(url);
+    const nextPokemon = await response.json();
+
+    setOffet(next ? offset + 20 : offset - 20);
+    setPokemon(nextPokemon);
+  };
 
   return (
-    <>
-     
-    </>
+    <Layout>
+      <div className="grid grid-col-1 md:grid-cols-3 lg:grid-cols-5 gap-10">
+        {pokemon.results.map((monster, index) => (
+          <Pokemon key={index} pokemon={monster} index={index + offset} />
+        ))}
+      </div>
+
+      <div className="mt-10 flex justify-center gap-5">
+        <button
+          disabled={!pokemon.previous}
+          className="disabled:bg-gray-500 px-3 py-1 bg-slate-900"
+          onClick={() => fetchPokemon(pokemon.previous, false)}
+        >
+          Prev
+        </button>
+        <button
+          disabled={!pokemon.next}
+          className="disabled:bg-gray-500 px-3 py-1 bg-slate-900"
+          onClick={() => fetchPokemon(pokemon.next, true)}
+        >
+          Next
+        </button>
+      </div>
+    </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
-  const response  = await fetch("https://pokeapi.co/api/v2/pokemon")
-  const initialPokemon = await response.json()
-
-  // let arrayPokemon = [];
-
-  // for (let i = 1; i < !20; i++) {
-  //   let data = await cathPokemon(i);
-  //   arrayPokemon.push(data);
-  // }
-
-  // let initialPokemon = arrayPokemon.map((pokemon) => {
-  //   return {
-  //     id: pokemon.id,
-  //     name: pokemon.name,
-  //     image: pokemon.sprites.other.dream_world.front_default,
-  //     types: pokemon.types,
-  //   };
-  // });
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const initialPokemon = await response.json();
 
   return {
     props: {
       initialPokemon,
-    }
-  }
+    },
+  };
 }
